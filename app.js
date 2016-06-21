94c8aee0a913dbe719b0bd79df1b8fd4d4f8b435
hanmiton <hanmilton_12@outlook.com>
2016-06-21 05:35:05 -0500

    agregando mongoose y schema y insertando priemr elemento

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var app = express();
 
-app.set('view engine','jade');
+mongoose.connect("mongodb://localhost/primera");
 
-app.use(express.static('public'));
+//definir el schema de nuestro sproductos
 
-app.get('/',function(req,res){
-	res.render('index');
+var productSchema = {
+	title:String,
+	descripiton:String,
+	imageUrl:String,
+	pricing: Number
+};
+
+var Product = mongoose.model("Product", productSchema);
+
+
+
+app.set("view engine","jade");
+
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+/*	var data = {
+		title : "Mi primer super producto",
+		descripiton : "Una mega super hiper compra",
+		imageUrl : "data.png",
+		pricing: 10
+	}
+
+	var product = new Product(data);
+
+	product.save(function(err){
+		console.log(product);
+	});*/
+	res.render("index");
 });
 
-app.listen('8080');
+app.listen("8080");
